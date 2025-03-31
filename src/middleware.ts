e5db13c86e3b612964ed29d0db3cb63017198975
Renan <renancossenzo@hotmail.com>
2025-03-30 22:19:39 -0300

    Comment middleware protected pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-    console.log("test")
     const token = req.cookies.get("authToken");
 
-    const protectedRoutes = ["/sign-up"];
+    // const protectedRoutes = ["/sign-up"];
 
-    if (protectedRoutes.includes(req.nextUrl.pathname) && !token) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
+    // if (protectedRoutes.includes(req.nextUrl.pathname) && !token) {
+    //     return NextResponse.redirect(new URL("/login", req.url));
+    // }
 
     return NextResponse.next();
 }
